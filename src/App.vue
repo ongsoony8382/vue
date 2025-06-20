bb7143bf474b1abe81c2c97f85ae1ef5b24906a6
ongsoony8382 <ongsoony8382@gmail.com>
2025-06-20 13:06:48 +0900

    vue 06-20

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
   <h1>App.vue</h1>
   <router-link to="/component">컴포넌트</router-link>
   <router-link to="/mustache">콧수염 템플릿</router-link>
+  <router-link to="/Chapter3">chapter3</router-link>
+  <router-link to="/Chapter4">chapter4</router-link>
+  <router-link to="/Chapter5">반응형 상태</router-link>
   <router-view />  
 </template>
 
---
+++ src/views/Chapter3.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+  
+   import { ref } from 'vue';
+   const fruits = 'apple avocado';
+   const fruitArr = ['banana', 'blueberry'];
+   const visible = ref({fruit: true, pointer: true});
+   const hidden = {fruit: false};
+
+   const changeVisible = () => {
+    visible.value.fruit = !visible.value.fruit;
+   }
+
+   // 데이터가 많은데 문자열 ... 지양!
+   const rect = 'background-color: red; width: 200px; height: 100px;'
+   // 대신 객체 활용. 수정할 때 편함. 
+   const rectObj = {
+    backgroundColor: 'red',
+    width: '200px',
+    height: '100px',
+    marginTop: '10px'
+   };
+   
+   let loading = true;
+</script>
+
+<template>
+    <h1>Chapter3.vue</h1>
+    <div :class="fruits">사과 / 아보카도</div>
+    <div :class="fruitArr" :title="fruitArr">바나나 / 블루베리</div>
+    <hr>
+    <div :class="visible" class="pointer" @click="changeVisible">사과 {{ visible.fruit }}</div>
+    <div :class="hidden">오이</div>
+    <div :style="rect">rect</div>
+    <div :style="rectObj">rectObj</div>
+    <div v-if="loading">로딩 중</div>
+    <div v-else>로딩 끝!!</div>
+
+    <div v-show="loading">show loading</div>
+</template>
+
+<style scoped>
+.fruit{color: red;}
+</style>
